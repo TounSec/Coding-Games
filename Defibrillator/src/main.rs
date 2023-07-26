29a69604f0595938fd93fdf62e78b7c43bcd4fdf
TounSec <hackethique@gmail.com>
2023-07-26 21:13:33 +0200

    Puzzle : Defibrillator

--- Defibrillator/src/main.rs
+++ Defibrillator/src/main.rs
@@ -14,25 +14,21 @@ macro_rules! parse_input {
  **/
 fn main() {
     // Input long
-    println!("Select long: ");
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let user_lon = input_line.trim().replace(",", ".").parse::<f64>().unwrap();
 
     // Input lat
-    println!("\nSelect lat: ");
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let user_lat = input_line.trim().replace(",", ".").parse::<f64>().unwrap();
 
     // Input nb defibrillator
-    println!("\nDefibrillator number: ");
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let n = parse_input!(input_line, i32);
 
     // Input defibrillator description (|ID|Name|Address|Number phone|Lon|Lat|)
-    println!("\nSelect defibrillator description: ");
     let mut descriptions = Vec::new();
     for _ in 0..n {
         let mut input_line = String::new();
