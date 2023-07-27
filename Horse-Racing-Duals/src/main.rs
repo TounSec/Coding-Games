90948e0537f93c91a2765f912896ac8e1699df83
TounSec <hackethique@gmail.com>
2023-07-27 20:47:29 +0200

    Puzzle : Horse Racing Duals

--- Horse-Racing-Duals/src/main.rs
+++ Horse-Racing-Duals/src/main.rs
@@ -1,3 +1,36 @@
+use std::io;
+
+macro_rules! parse_input {
+    ($x:expr, $t:ident) => ($x.trim().parse::<$t>().unwrap())
+}
+
+/**
+    The Hippodrome de Casablanca organizes a new type of horse racing : In a duel, only two horses participate in the race. For the race to be interesting, it is necessary to select two horses that have a similar power.
+    Write a program that, given a set of powers, identifies the two closest powers and displays their difference with a positive integer.
+ **/
 fn main() {
-    println!("Hello, world!");
+    // Input number horse
+    println!("Number 🐎 : ");
+    let mut input_line = String::new();
+    io::stdin().read_line(&mut input_line).unwrap();
+    let n = parse_input!(input_line, i32);
+
+    println!("{}", n);
+
+    // Input pi horses
+    let mut values_pi: Vec<i32> = Vec::new();
+
+    for i in 0..n as usize {
+        println!("Pi value 🐎 {} : ", i+1);
+        let mut input_line = String::new();
+        io::stdin().read_line(&mut input_line).unwrap();
+        let pi = parse_input!(input_line, i32);
+        values_pi.push(pi);
+    }
+
+    println!("{:?}", values_pi);
+    // Write an answer using println!("message...");
+    // To debug: eprintln!("Debug message...");
+
+    println!("answer");
 }
