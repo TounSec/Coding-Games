09fec2b16c8383b15572f17793eabd22336a9141
TounSec <hackethique@gmail.com>
2023-07-28 21:10:23 +0200

    Puzzle : Shadows of the Knight - Episode 1

--- Shadows-Of-The-Knight-Episode-1/src/main.rs
+++ Shadows-Of-The-Knight-Episode-1/src/main.rs
@@ -9,33 +9,50 @@ macro_rules! parse_input {
  * the standard input according to the problem statement.
  **/
 fn main() {
+    // Input width & height
+    println!("Number of windows : ");
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let inputs = input_line.split(" ").collect::<Vec<_>>();
-    let w = parse_input!(inputs[0], i32); // width of the building.
-    let h = parse_input!(inputs[1], i32); // height of the building.
+    let w = parse_input!(inputs[0], i32);
+    let h = parse_input!(inputs[1], i32);
+    println!("The building has a width of {} and a height of {}", w, h);
 
+    // Input maximum number of turns before game over
+    println!("Number of jumps : ");
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
-    let n = parse_input!(input_line, i32); // maximum number of turns before game over.
+    let mut n = parse_input!(input_line, i32);
+    println!("You have to find the bombs in {} jumps", n);
 
+    // Start position
+    println!("x, y Coordinates of starting position : ");
     let mut input_line = String::new();
     io::stdin().read_line(&mut input_line).unwrap();
     let inputs = input_line.split(" ").collect::<Vec<_>>();
     let x0 = parse_input!(inputs[0], i32);
     let y0 = parse_input!(inputs[1], i32);
+    println!("You start in position ({},{})", x0, y0);
 
     // game loop
     loop {
-        let mut input_line = String::new();
-        io::stdin().read_line(&mut input_line).unwrap();
-        let bomb_dir = input_line.trim().to_string(); // the direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+        if n > 0 {
+            // Input direction of the bombs from batman's current location (U, UR, R, DR, D, DL, L or UL)
+            println!("Direction of the bombs : ");
+            let mut input_line = String::new();
+            io::stdin().read_line(&mut input_line).unwrap();
+            let bomb_dir = input_line.trim().to_string();
+            println!("The hostages are located towards {}", bomb_dir);
+            n -= 1;
 
-        // Write an action using println!("message...");
-        // To debug: eprintln!("Debug message...");
+        } else {
+            println!("😵🕹️GAME OVER🕹️😵");
+            break;
+        }
+
 
 
         // the location of the next window Batman should jump to.
-        println!("0 0");
+        // println!("0 0");
     }
 }
